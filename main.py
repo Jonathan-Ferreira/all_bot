8998df0fcb5fe539a2c7eb309fd408174971421c
Jonathan-Ferreira <jonathandferreira@outlook.com>
2024-10-22 17:13:14 -0300

    Correção dos comentários

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # Importa o discord.py, permitindo o acesso a API do discord
 import discord
-# Gerenciador de erros de Timeout
-import asyncio  
+
 # Importa o módulo de OS
 import os
 
@@ -13,7 +12,7 @@ channel_list = []
 intents = discord.Intents.default() # Usa as intenções padrões do Bot.
 intents.message_content = True   # Permite que o Bot leia o conteúdo de mensagens
 
-# Obtém o token da API localizado no arquivo .env
+# Obtém o token da API na variável local DISCORD_TOKEN
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 if DISCORD_TOKEN is None:
 	raise ValueError("Token não localizado.")
@@ -21,6 +20,7 @@ if DISCORD_TOKEN is None:
 # Obtém o objeto do cliente do discord.py. O cliente é sinônimo do bot
 bot = discord.Client(intents=intents)
 
+# Função para gerenciar o acesso do bot aos canais de voz.
 @bot.event
 async def voice_channel(ctx,message):
 	if message == "juntar":
@@ -55,30 +55,33 @@ async def on_ready():
 # Listener de evento que ativa quando uma mensagem é enviada ao canal
 @bot.event
 async def on_message(message):
+	# Match para validar o tipo de mensagem enviada e decidir qual tarefa deve ser realizada.
 	match message.content:
-	# lista de IFs para checar o conteúdo da mensagem
 		case "oi":
 			await message.channel.send("Olá")
-		
-		case "fechar":
+				
+		case "fechar":			
 			await message.channel.send("Fechando")
+			# Por algum motivo esse comando trigga um aviso, porém está funcional.
 			await bot.close()
-
+		
+		# Apresenta a listagem de canais do servidor.
 		case "lista":
 			global channel_list
 			if channel_list == []:
 				await message.channel.send("Lista está vazia")
 			else:
 				await message.channel.send(f"Canais: {channel_list}")
-
+		
+		# Obtém a listagem de canais do servidor
 		case "servers":
 			guild = message.guild
 			channel_list = [(channel.name, channel.id) for channel in guild.channels if not isinstance(channel,discord.CategoryChannel)]
 			channel_info = '\n'.join([f"Canal: {name} | ID: {channel_id}" for name, channel_id in channel_list])
 			await message.channel.send(f"Canais:\n{channel_info}")
-
+	
+		# Apresenta a listagem de canais do servidor e seus tipos (Voz, Texto, etc.)
 		case "tipo":
-			# await message.channel.send("Chegou aqui")	 
 			guild = message.guild
 			if channel_list == []:
 				await message.channel.send("Lista está vazia")	 
@@ -87,6 +90,7 @@ async def on_message(message):
 				channel_info = '\n'.join([f"Canal: {name} | ID: {channel_type}" for name, channel_type in channel_list])
 				await message.channel.send(f"Canais:\n{channel_info}")
 
+		# Faz um chamado na função de tratamento para conexão com canais de voz.
 		case "juntar"  | "desconectar":
 			msg = message.content
 			await message.channel.send(f"Info:\n{msg}")
