0bf19e0701f9028e8a55bb70b49d93b8f4104cb2
Jonathan-Ferreira <jonathandferreira@outlook.com>
2024-10-22 10:03:38 -0300

    Adicionando o Token Pra testar o código

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ channel_list = []
 intents = discord.Intents.default() # Usa as intenções padrões do Bot.
 intents.message_content = True   # Permite que o Bot leia o conteúdo de mensagens
 
-# Carrega o arquivo .env no mesmo diretório do código
-load_dotenv(dotenv_path='C:/Users/SDE/Desktop/Teste Git/discord_bot/DISCORD_TOKEN.env')
+DISCORD_TOKEN = "MTI5NTcxODMwOTE2MDk0Nzc5NQ.Gs8FAx.YegUu3HgdxqwVUDqKI9Qno4AwoRkZLwUWI8jD8"
 
 # Obtém o token da API localizado no arquivo .env
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+# DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+# if DISCORD_TOKEN is None:
+# 	raise ValueError("Token não localizado.")
 
 # Obtém o objeto do cliente do discord.py. O cliente é sinônimo do bot
 bot = discord.Client(intents=intents)
@@ -49,6 +50,7 @@ async def on_message(message):
 	if message.content == "fechar":
 		await message.channel.send("Fechando")
 		await bot.close()
+		
 	if message.content == "lista":
 		global channel_list
 		if channel_list == []:
@@ -59,7 +61,7 @@ async def on_message(message):
 	if message.content == "servers":
 		guild = message.guild
 		channel_list = [(channel.name, channel.id) for channel in guild.channels if not isinstance(channel,discord.CategoryChannel)]
-		channel_info = '\n'.join([f"Channel name: {name} | Channel ID: {channel_id}" for name, channel_id in channel_list])
+		channel_info = '\n'.join([f"Canal: {name} | ID: {channel_id}" for name, channel_id in channel_list])
 		await message.channel.send(f"Canais:\n{channel_info}")
 
 	if message.content == "tipo":
@@ -69,10 +71,11 @@ async def on_message(message):
 			await message.channel.send("Lista está vazia")	 
 		else:
 			channel_list = [(channel.name, channel.type) for channel in guild.channels if not isinstance(channel,discord.CategoryChannel)]
-			channel_info = '\n'.join([f"Channel name: {name} | Channel Type: {channel_type}" for name, channel_type in channel_list])
+			channel_info = '\n'.join([f"Canal: {name} | ID: {channel_type}" for name, channel_type in channel_list])
 			await message.channel.send(f"Canais:\n{channel_info}")
 
 	if message.content == "juntar":
+		await message.channel.send(f"Canais:\n{channel_list}")
 		await message.channel.send("Qual canal deseja se juntar(nome)?")
 		try:
 			msg = await bot.wait_for("message",timeout=3.0)
@@ -88,7 +91,6 @@ async def on_message(message):
 	
 	if message.content == "desconectar":
 		await bot.disconnect()
-		e
 
 # Executa o bot com o token especificado.
 bot.run(DISCORD_TOKEN)
