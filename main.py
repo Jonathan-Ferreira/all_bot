e4479321122a2a8909ede5eb7ba138211aa17d2a
Jonathan-Ferreira <jonathandferreira@outlook.com>
2024-10-23 11:18:24 -0300

    Modularização do tratamento de comandos/mensagem (Adicionar o código para o comando !help)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Importa o discord.py, permitindo o acesso a API do discord
 import discord
+from discord.ext import commands
 
 # Importa o módulo de OS
 import os
@@ -18,22 +19,82 @@ if DISCORD_TOKEN is None:
 	raise ValueError("Token não localizado.")
 
 # Obtém o objeto do cliente do discord.py. O cliente é sinônimo do bot
-bot = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!",intents=intents)
 
-# Função para gerenciar o acesso do bot aos canais de voz.
-@bot.event
-async def voice_channel(ctx,message):
-	if message == "juntar":
+
+class VoiceHandler:
+	def __init__(self,bot):
+		self.bot = bot
+
+	async def juntar(self,ctx):
 		if ctx.author.voice:
-			channel = ctx.author.voice.channel
-			await channel.connect()
-			await ctx.channel.send(f'Juntando ao canal {channel}')
-	elif message == "desconectar":
+			canal = ctx.author.voice.channel
+			await canal.connect()
+			await ctx.channel.send(f'Juntando ao canal {canal}')			
+		else:
+			await ctx.channel.send('Você não está em um canal de voz')
+
+	async def sair(self,ctx):
 		if ctx.voice_client:
-			await channel.voice_client.disconnect()
+			await ctx.voice_client.disconnect()
 			await ctx.channel.send(f'Saindo...')
-	else:
-		await ctx.channel.send('Você não está em um canal de voz')
+		else:
+			await ctx.channel.send("Não estou conectado em um canal")
+
+class MessageHandler:
+	def __init__(self,bot):
+		self.bot = bot
+
+	async def gerenciador_msg(self,ctx):
+		# Match para validar o tipo de mensagem enviada e decidir qual tarefa deve ser realizada.
+
+		match ctx.message.content:
+			case "oi":
+				await ctx.channel.send("Olá")
+					
+			case "fechar":			
+				await ctx.channel.send("Fechando")
+				# Por algum motivo esse comando trigga um aviso, porém está funcional.
+				await bot.close()
+			
+			# Apresenta a listagem de canais do servidor.
+			case "lista":
+				global channel_list
+				if channel_list == []:
+					await ctx.channel.send("Lista está vazia")
+				else:
+					await ctx.channel.send(f"Canais: {channel_list}")
+			
+			# Obtém a listagem de canais do servidor
+			case "servers":
+				guild = ctx.guild
+				channel_list = [(channel.name, channel.id) for channel in guild.channels if not isinstance(channel,discord.CategoryChannel)]
+				channel_info = '\n'.join([f"Canal: {name} | ID: {channel_id}" for name, channel_id in channel_list])
+				await ctx.channel.send(f"Canais:\n{channel_info}")
+		
+			# Apresenta a listagem de canais do servidor e seus tipos (Voz, Texto, etc.)
+			case "tipo":
+				guild = ctx.guild
+				if channel_list == []:
+					await ctx.channel.send("Lista está vazia")	 
+				else:
+					channel_list = [(channel.name, channel.type) for channel in guild.channels if not isinstance(channel,discord.CategoryChannel)]
+					channel_info = '\n'.join([f"Canal: {name} | ID: {channel_type}" for name, channel_type in channel_list])
+					await ctx.channel.send(f"Canais:\n{channel_info}")
+
+	
+
+voice_handler = VoiceHandler(bot)
+message_handler = MessageHandler(bot)
+
+
+@bot.command()
+async def juntar(ctx):
+	await voice_handler.juntar(ctx)
+
+@bot.command()
+async def sair(ctx):
+	await voice_handler.sair(ctx)
 
 # Listener de evento que ativa quando o bot é ligado
 @bot.event
@@ -52,49 +113,27 @@ async def on_ready():
 	# Imprime a quantidade de servidores que o bot está conectado
 	print("Teste está em " + str(guild_count) + " servers.")
 
-# Listener de evento que ativa quando uma mensagem é enviada ao canal
+# Gerenciar erros
 @bot.event
-async def on_message(message):
-	# Match para validar o tipo de mensagem enviada e decidir qual tarefa deve ser realizada.
-	match message.content:
-		case "oi":
-			await message.channel.send("Olá")
-				
-		case "fechar":			
-			await message.channel.send("Fechando")
-			# Por algum motivo esse comando trigga um aviso, porém está funcional.
-			await bot.close()
-		
-		# Apresenta a listagem de canais do servidor.
-		case "lista":
-			global channel_list
-			if channel_list == []:
-				await message.channel.send("Lista está vazia")
-			else:
-				await message.channel.send(f"Canais: {channel_list}")
-		
-		# Obtém a listagem de canais do servidor
-		case "servers":
-			guild = message.guild
-			channel_list = [(channel.name, channel.id) for channel in guild.channels if not isinstance(channel,discord.CategoryChannel)]
-			channel_info = '\n'.join([f"Canal: {name} | ID: {channel_id}" for name, channel_id in channel_list])
-			await message.channel.send(f"Canais:\n{channel_info}")
-	
-		# Apresenta a listagem de canais do servidor e seus tipos (Voz, Texto, etc.)
-		case "tipo":
-			guild = message.guild
-			if channel_list == []:
-				await message.channel.send("Lista está vazia")	 
-			else:
-				channel_list = [(channel.name, channel.type) for channel in guild.channels if not isinstance(channel,discord.CategoryChannel)]
-				channel_info = '\n'.join([f"Canal: {name} | ID: {channel_type}" for name, channel_type in channel_list])
-				await message.channel.send(f"Canais:\n{channel_info}")
+async def on_command_error(ctx, erro):
+    # Check if the error is because the command was not found
+    if isinstance(erro, commands.CommandNotFound):
+        await ctx.send(f"{ctx.message.content} é um comando inválido. Caso possua alguma dúvida, digitar '!ajuda' para obter a lista de todos os comandos disponíveis.")
+    else:
+        # Handle other types of errors (if any)
+        await ctx.send(f"Um erro ocorreu: {str(erro)}")
+
+
+@bot.event
+async def on_message(mensagem):
+	ctx = await bot.get_context(mensagem)
+	await message_handler.gerenciador_msg(ctx)
+	await bot.process_commands(mensagem)
+# Listener de evento que ativa quando uma mensagem é enviada ao canal
+
 
 		# Faz um chamado na função de tratamento para conexão com canais de voz.
-		case "juntar"  | "desconectar":
-			msg = message.content
-			await message.channel.send(f"Info:\n{msg}")
-			await voice_channel(message,msg)
+		 # Process commands after your custom logic
 		
 # Executa o bot com o token especificado.
 bot.run(DISCORD_TOKEN)
